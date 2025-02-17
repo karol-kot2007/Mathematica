526da83f76083ef7a48205cf18de74e180cde7dc
karol-kot2007 <karol.kot.2007@gmail.com>
2025-02-17 19:19:58 +0100

    wip

---
+++ Mathematica/FunctionSelector.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Mathematica
+{
+  partial class FunctionSelector
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      comboBox1 = new ComboBox();
+      button1 = new Button();
+      userControl11 = new UserControl1();
+      SuspendLayout();
+      // 
+      // comboBox1
+      // 
+      comboBox1.FormattingEnabled = true;
+      comboBox1.Location = new Point(192, 57);
+      comboBox1.Name = "comboBox1";
+      comboBox1.Size = new Size(121, 23);
+      comboBox1.TabIndex = 0;
+      comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+      // 
+      // button1
+      // 
+      button1.Location = new Point(237, 143);
+      button1.Name = "button1";
+      button1.Size = new Size(75, 23);
+      button1.TabIndex = 1;
+      button1.Text = "button1";
+      button1.UseVisualStyleBackColor = true;
+      // 
+      // userControl11
+      // 
+      userControl11.Location = new Point(3, 143);
+      userControl11.Name = "userControl11";
+      userControl11.Size = new Size(150, 150);
+      userControl11.TabIndex = 2;
+      // 
+      // FunctionSelector
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      Controls.Add(userControl11);
+      Controls.Add(button1);
+      Controls.Add(comboBox1);
+      Name = "FunctionSelector";
+      Size = new Size(432, 471);
+      Load += FunctionSelector_Load;
+      ResumeLayout(false);
+    }
+
+    #endregion
+
+    private ComboBox comboBox1;
+    private Button button1;
+    private UserControl1 userControl11;
+  }
+}
---
+++ Mathematica/LinearFunction.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Mathematica
+{
+  partial class LinearFunction
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      label1 = new Label();
+      label2 = new Label();
+      textBox1 = new TextBox();
+      textBox2 = new TextBox();
+      SuspendLayout();
+      // 
+      // label1
+      // 
+      label1.AutoSize = true;
+      label1.Location = new Point(63, 56);
+      label1.Name = "label1";
+      label1.Size = new Size(13, 15);
+      label1.TabIndex = 0;
+      label1.Text = "a";
+      label1.Click += label1_Click;
+      // 
+      // label2
+      // 
+      label2.AutoSize = true;
+      label2.Location = new Point(62, 88);
+      label2.Name = "label2";
+      label2.Size = new Size(14, 15);
+      label2.TabIndex = 1;
+      label2.Text = "b";
+      // 
+      // textBox1
+      // 
+      textBox1.Location = new Point(101, 53);
+      textBox1.Name = "textBox1";
+      textBox1.Size = new Size(100, 23);
+      textBox1.TabIndex = 3;
+      textBox1.TextAlign = HorizontalAlignment.Center;
+      // 
+      // textBox2
+      // 
+      textBox2.Location = new Point(101, 85);
+      textBox2.Name = "textBox2";
+      textBox2.Size = new Size(100, 23);
+      textBox2.TabIndex = 4;
+      textBox2.TextAlign = HorizontalAlignment.Center;
+      // 
+      // LinearFunction
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      Controls.Add(textBox2);
+      Controls.Add(textBox1);
+      Controls.Add(label2);
+      Controls.Add(label1);
+      Name = "LinearFunction";
+      Size = new Size(433, 299);
+      ResumeLayout(false);
+      PerformLayout();
+    }
+
+    #endregion
+
+    private Label label1;
+    private Label label2;
+    private TextBox textBox1;
+    private TextBox textBox2;
+  }
+}
